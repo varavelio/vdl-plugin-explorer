f92bf2c2339a0a8fe4f89155bdde645a47996b5e
Luis Eduardo <eduardo.devop@gmail.com>
2026-04-22 01:32:59 +0000

    feat: restructure layout to include AppLayout and ThemePicker components

--- explorer/src/routes/+layout.svelte
+++ explorer/src/routes/+layout.svelte
@@ -1,8 +1,53 @@
 <script lang="ts">
   import "../app.css";
-  import { UiProvider } from "@varavel/ui";
+  import { ThemePicker, UiProvider } from "@varavel/ui";
+  import { AppLayout } from "@varavel/ui/layouts";
+  import { viewport } from "@varavel/ui/runtime";
+
+  import Logo from "$lib/components/Logo.svelte";
 
   let { children } = $props();
 </script>
 
-<UiProvider>{@render children()}</UiProvider>
+<UiProvider>
+  <AppLayout primaryRegion="sidebar" maxWidth="md">
+    {#snippet sidebarTop()}
+      <header class="hidden w-full desk:block">
+        <a href="https://vdl.varavel.com" target="_blank">
+          <Logo class="mx-auto w-27.5 desk:w-30" />
+        </a>
+      </header>
+    {/snippet}
+
+    {#snippet sidebarCenter()}
+    <!--  -->
+    {/snippet}
+
+    {#snippet headerLeft()}
+    <!--  -->
+    {/snippet}
+
+    {#snippet headerCenter()}
+      <header class="w-full desk:hidden">
+        <a href="https://vdl.varavel.com" target="_blank">
+          <Logo class="mx-auto w-27.5 desk:w-30" />
+        </a>
+      </header>
+    {/snippet}
+
+    {#snippet headerRight()}
+      <div>
+        <ThemePicker
+          class={{"-mr-2 [&_svg]:size-5": viewport.isMobile}}
+          variant={viewport.isMobile ? "ghost" : "outline"}
+          showLabel={viewport.isDesktop}
+          square={viewport.isMobile}
+        />
+      </div>
+    {/snippet}
+
+    {#snippet main()}
+      {@render children()}
+    {/snippet}
+  </AppLayout>
+</UiProvider>
