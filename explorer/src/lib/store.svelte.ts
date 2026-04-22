332bd6fc65dec4f77dc09ff2ae555b9d601c05e4
Luis Eduardo <eduardo.devop@gmail.com>
2026-04-22 05:53:11 +0000

    feat: enhance Store class to load local IR schema in development mode

--- explorer/src/lib/store.svelte.ts
+++ explorer/src/lib/store.svelte.ts
@@ -1,6 +1,47 @@
 import type { IrSchema } from "@varavel/vdl-plugin-sdk";
 import { isEmptyObject } from "@varavel/vdl-plugin-sdk/utils/predicates";
+import { browser, dev } from "$app/environment";
 
+// In development mode, place your IR schema in a file named ir.local.json at the
+// root of the project. This allows you to work with a static IR schema without
+// needing to run the VDL plugin or have a backend server running.
+
+const EMPTY_IR: IrSchema = {
+  entryPoint: "",
+  types: [],
+  enums: [],
+  constants: [],
+  docs: [],
+};
+
+/**
+ * Find the local IR mock file named ir.local.json at the root of the project and
+ * return its content as an IrSchema.
+ *
+ * This is used in development mode to load a static IR schema without needing to
+ * run the VDL plugin or have a backend server running.
+ *
+ * @returns The IR schema loaded from the local JSON file, or an empty IR schema
+ * if the file is not found or cannot be loaded.
+ */
+function findLocalMock(): IrSchema {
+  if (!dev) return EMPTY_IR;
+
+  // Find the local IR mock file using Vite's import.meta.glob
+  const files = import.meta.glob("../../../ir.local.json", { eager: true });
+
+  // biome-ignore lint/suspicious/noExplicitAny: The type of the mock module is not known at compile time
+  const mockModule = files["../../../ir.local.json"] as any;
+  if (!mockModule?.default) return EMPTY_IR;
+
+  console.log("[DEV] Default IR loaded from ir.local.json");
+  return mockModule.default;
+}
+
+/**
+ * Declare the global interface for the expected window varriable injected
+ * by the VDL plugin at build time.
+ */
 declare global {
   interface Window {
     /**
@@ -15,21 +56,29 @@ declare global {
  * Store class holds all the application shared state.
  */
 export class Store {
+  /**
+   * Flag to indicate if the store has been initialized.
+   */
   initialized = $state(false);
 
-  ir: IrSchema = $state({
-    entryPoint: "",
-    types: [],
-    enums: [],
-    constants: [],
-    docs: [],
-  });
+  /**
+   * The IR schema generated from the VDL source code.
+   */
+  ir: IrSchema = $state(EMPTY_IR);
 
   constructor() {
     if (this.initialized) return;
-    if (typeof window === "undefined") return;
+    if (!browser) return;
 
-    if (!isEmptyObject(window.__vdl_ir)) {
+    // In development mode, load the IR from a local JSON file
+    // named ir.local.json at the root of the project.
+    if (dev) {
+      this.ir = findLocalMock();
+    }
+
+    // Load production IR from the global variable injected by the VDL plugin
+    // in the window object.
+    if (!dev && !isEmptyObject(window.__vdl_ir)) {
       this.ir = window.__vdl_ir;
     }
 
