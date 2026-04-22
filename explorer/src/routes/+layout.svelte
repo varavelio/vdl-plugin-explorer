a24370366e5f3da5b5eb3e848cf4b9df2f421b18
Luis Eduardo <eduardo.devop@gmail.com>
2026-04-22 16:19:25 +0000

    fix: adjust layout and styling in +layout.svelte, including SidebarCenter integration

--- explorer/src/routes/+layout.svelte
+++ explorer/src/routes/+layout.svelte
@@ -6,6 +6,7 @@
   import { viewport } from "@varavel/ui/runtime";
   import Logo from "$lib/components/Logo.svelte";
   import { store } from "$lib/store.svelte";
+  import SidebarCenter from "./components/SidebarCenter.svelte";
 
   let { children } = $props();
 </script>
@@ -16,11 +17,15 @@
   </div>
 {:else}
   <UiProvider>
-    <AppLayout primaryRegion="header" maxWidth="md">
+    <AppLayout
+      primaryRegion="header"
+      maxWidth="md"
+      showSidebarBottom={viewport.isMobile}
+    >
       {#snippet headerLeft()}
-        <header class="hidden not-only:w-full desk:block">
+        <header class="hidden not-only:w-full desk:block ml-2">
           <a href="https://vdl.varavel.com" target="_blank">
-            <Logo class="w-30" />
+            <Logo class="h-8" />
           </a>
         </header>
       {/snippet}
@@ -28,7 +33,7 @@
       {#snippet headerCenter()}
         <header class="w-full desk:hidden">
           <a href="https://vdl.varavel.com" target="_blank">
-            <Logo class="mx-auto w-27.5" />
+            <Logo class="mx-auto h-8" />
           </a>
         </header>
       {/snippet}
@@ -54,28 +59,22 @@
         </div>
       {/snippet}
 
-      {#snippet sidebarTop()}
-      <!--  -->
-      {/snippet}
-
       {#snippet sidebarCenter()}
-      <!--  -->
+        <SidebarCenter />
       {/snippet}
 
       {#snippet sidebarBottom()}
-        {#if viewport.isMobile}
-          <div class="w-full space-y-2">
-            <GithubButton
-              user="varavelio"
-              repo="vdl-plugin-explorer"
-              size="sm"
-              showStars={true}
-              showTag={false}
-              showForks={false}
-              wide={true}
-            />
-          </div>
-        {/if}
+        <div class="w-full space-y-2">
+          <GithubButton
+            user="varavelio"
+            repo="vdl-plugin-explorer"
+            size="sm"
+            showStars={true}
+            showTag={false}
+            showForks={false}
+            wide={true}
+          />
+        </div>
       {/snippet}
 
       {#snippet main()}
