04045b8387f1aa96441dec50d51939788c2e833e
Luis Eduardo <eduardo.devop@gmail.com>
2026-04-22 00:26:39 +0000

    feat: add configuration for SvelteKit to enable SPA with CSR

---
+++ explorer/src/routes/+layout.ts
@@ -0,0 +1,5 @@
+// Configure SvelteKit to generate a Single Page Application (SPA)
+// by disabling SSR and enabling CSR.
+export const prerender = true;
+export const ssr = false;
+export const csr = true;
